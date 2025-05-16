be9927a3bbe1ea58413350d74861fc2208a3cc32
Rainman1980X <R@inman123>
2025-05-16 07:07:50 +0200

    Add Jackson and Lombok dependencies to build.gradle.kts
    Jackson dependencies are added for JSON processing, while Lombok is added to reduce boilerplate code. Testing dependencies for Lombok and JUnit are also included to support test environments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.testImplementation
+
 plugins {
     kotlin("jvm") version "2.1.20"
 }
@@ -10,6 +12,20 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
+
+    // Lombok (aktuelle Version)
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    // Optional: Lombok im Testcode
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
     implementation("com.formdev:flatlaf:3.3")
     implementation("com.miglayout:miglayout-swing:11.0")
     testImplementation(kotlin("test"))
